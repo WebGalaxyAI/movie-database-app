e8714b4fb2b0893f35f37e1935e0be423ab02742
vitaliishevchuk <vitaliishevchuk123@gmail.com>
2025-01-11 19:38:05 +0200

    clear scripts

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -25,35 +25,5 @@
 </div>
 @livewireScripts
 @vite(['resources/js/app.js'])
-<script>
-    // Функція для встановлення куки
-    function setCookie(name, value, days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        document.cookie = name + "=" + value + "; expires=" + date.toUTCString() + "; path=/";
-    }
-
-    // Функція для отримання куки за ім'ям
-    function getCookie(name) {
-        const nameEQ = name + "=";
-        const ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    }
-
-    // Функція для оновлення теми
-    function setTheme() {
-        console.log(123)
-        if (document.getElementById("toggle").checked) {
-            setCookie('theme', 'dark', 365); // Зберігаємо в куки вибір теми
-        } else {
-            setCookie('theme', 'light', 365); // Зберігаємо в куки вибір теми
-        }
-    }
-</script>
 </body>
 </html>
